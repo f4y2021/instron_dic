abde6e31a93ad84626e91edd7bdabcbe813e7a13
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2021-12-28 15:51:17 +0000

    Update Instron.py

--- Instron.py
+++ Instron.py
@@ -263,7 +263,7 @@ if run_button:
     streamtest = save_virtual_workbook(wb)
     st.download_button("Download Final Excel File",streamtest,'Tabela_Final_'+str(sample_name)+'.xlsx')
 
-
+st.markdown('##')
 with st.expander("License"):
     st.text("""THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
